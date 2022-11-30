3aff1d1168cd6cf96e21f1782d4cc97696c4526b
bengugultekin <bengunurgultekin54@gmail.com>
2022-11-30 22:27:22 +0300

    added main component structure and created basic navbar with react router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
 import reportWebVitals from './reportWebVitals';
+import "./App.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import * as serviceWorker from './serviceWorker';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<AppRouter/>, document.getElementById('root'));
+
+serviceWorker.unregister();
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <AppRouter />
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
